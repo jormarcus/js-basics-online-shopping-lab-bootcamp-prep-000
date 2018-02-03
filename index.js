cb2b735e309984384f15a0a8f5a8e61432eaedd6
Jordan Marcus <jormarcus92@gmail.com>
2018-02-03 17:32:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,24 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * (100) + 1);
+  cart.push({[item]: price});
+  console.log(`${item} has been added to your cart.`)
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var str = 'In your cart, you have ';
+  if (cart.length === 0) {
+    return 'Your shopping cart is empty.';
+  }
+  for (var i = 0; i < cart.length; i++) {
+     if (i < cart.length - 1) {
+       str += `${item} at $ ${price},`
+     } else {
+       str += `${item} at $ ${price}.`
+     }
+  }
 }
 
 function total() {
